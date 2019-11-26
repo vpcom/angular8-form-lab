ff7b770f71ec198e8b7bbd507f27560306f30077
Vincent Perrin <vpcom01@gmail.com>
2019-11-26 10:47:11 +0100

    feat(intro): added intro component and first text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 
-const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'intro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'intro',
+    component: IntroComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
